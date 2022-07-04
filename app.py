a08cdaf2c9ffc897d0886cfda019db26b2abd666
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-07-03 18:39:59 -0600

    added database config

--- app.py
+++ app.py
@@ -24,10 +24,11 @@ bcrypt = Bcrypt()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///capstone'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgresql:///capstone')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "topsecret1"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "nevertell")
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 # debug = DebugToolbarExtension(app)
 
