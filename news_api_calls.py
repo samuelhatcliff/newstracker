39f50d2be4c5551b3182c4da492a973a28544687
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-09-29 16:29:45 -0600

    removed creds when not in prod

--- news_api_calls.py
+++ news_api_calls.py
@@ -6,8 +6,13 @@ from newsapi.newsapi_client import NewsApiClient
 from dateutil import parser
 # newsApi imports
 from newsapi.newsapi_client import NewsApiClient
-import creds
-my_api_key = os.environ.get("API_KEY", creds.api_key)
+production = True
+if not production:
+    import creds
+    my_api_key = os.environ.get("API_KEY", creds.api_key)
+else:
+    my_api_key = os.environ.get("API_KEY")
+
 newsapi = NewsApiClient(api_key=my_api_key)
 #async imports
 from multiprocessing.dummy import Pool as ThreadPool
